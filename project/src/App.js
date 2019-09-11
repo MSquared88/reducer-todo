f33e5bcd019ba88c12b77896f5bf5e7e0a6ee94b
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-09-10 21:25:50 -0500

    added moment functionallity

--- project/src/App.js
+++ project/src/App.js
@@ -1,16 +1,14 @@
-import React, { useReducer} from 'react';
+import React from 'react';
+
 
 //styling
 import './App.css';
-
-// state
-import { initialState, todoReducer } from "./reducers/todoReducer";
 
 // components
 import Todo from './component/Todo'
 
 function App() {
-  const [state, dispatch] = useReducer(todoReducer, initialState);
+
   return (
     <div className="App">
       <Todo/>
--- project/src/component/TodoItems.js
+++ project/src/component/TodoItems.js
@@ -1,13 +1,24 @@
-import React, {useReducer} from 'react'
+import React  from 'react'
+import Moment from 'react-moment'
+import 'moment-timezone';
 
 function TodoItems(props) {
   
   return (
-    <div 
-    className= {`todo${props.todo.completed ? '-completed' : '' }`}
-    onClick= {() => props.toggleCompleted(props.todo.id)}
-    >
-      <h3>{props.todo.item}</h3>
+    <div className= 'todo-container'>
+      <div 
+      className= {`todo${props.todo.completed ? '-completed' : '' }`}
+      onClick= {() => props.toggleCompleted(props.todo.id)}
+      >
+        <h3>{props.todo.item}</h3>
+      </div>
+        {(!props.todo.completed)
+        ? <span></span>
+        
+        : <div className= 'date-completed'>
+            <p><b>Date Completed:</b> <Moment format="YYYY/MM/DD " >{new Date}</Moment></p>
+          </div>}
+      
     </div>
   )
 }
